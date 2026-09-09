d81f6b9a7d582c44ef9537e2f964ba30b0af6426
Akshat Shrivastava <akshat1feb@gmail.com>
2026-09-09 23:24:23 +0530

    Time: 957 ms (6.37%) | Memory: 30.3 MB (66.30%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -6,7 +6,16 @@ public:
     nums[i] = nums[i] * nums[i];
 }
 
-sort(nums.begin(), nums.end());
+ for (int i = 0; i < nums.size(); i++) {
+            for (int j = 0; j < nums.size() - i - 1; j++) {
+
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
 
 return nums;
     }
