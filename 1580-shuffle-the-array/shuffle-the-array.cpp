35723ae290f8b4cdc630f2da2511aa79969ea109
Akshat Shrivastava <akshat1feb@gmail.com>
2026-09-12 00:40:31 +0530

    Time: 4 ms (65.55%) | Memory: 13.5 MB (19.63%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.cpp
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> ans(nums.size());
-        int i= 0;
+        int i = 0;
         int j = n;
         int k= 0;
         while(k < n){
